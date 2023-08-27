86931460da3dc160060960befc7a1875e0a6cfa2
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2023-08-27 14:27:46 +0300

    WhoWeAreDetail Api

---
+++ HouseForSale_Api/DTOs/WhoWeAreDetailDTOs/GetByIDWhoWeAreDetailDto.cs
@@ -0,0 +1,11 @@
+﻿namespace HouseForSale_Api.DTOs.WhoWeAreDetailDTOs
+{
+    public class GetByIDWhoWeAreDetailDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Description1 { get; set; }
+        public string Description2 { get; set; }
+    }
+}
---
+++ HouseForSale_Api/Repositories/CategoryRepository/Abstract/IWhoWeAreRepository.cs
@@ -0,0 +1,13 @@
+﻿using HouseForSale_Api.DTOs.WhoWeAreDetailDTOs;
+
+namespace HouseForSale_Api.Repositories.CategoryRepository.Abstract
+{
+    public interface IWhoWeAreDetailRepository
+    {
+        Task<List<ResultWhoWeAreDetailDto>> GetAllWhoWeAreDetailAsync();
+        void CreateWhoWeAreDetail(CreateWhoWeAreDetailDto createWhoWeAreDetailDto);
+        void DeleteWhoWeAreDetail(int id);
+        void UpdateWhoWeAreDetail(UpdateWhoWeAreDetailDto updateWhoWeAreDetailDto);
+        Task<GetByIDWhoWeAreDetailDto> GetWhoWeAreDetail(int id);
+    }
+}
