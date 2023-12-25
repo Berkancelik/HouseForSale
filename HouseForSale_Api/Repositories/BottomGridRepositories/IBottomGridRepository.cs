426bcadabef04985b72410f80dd10d836b2f31a7
berkancelik <celikberkan1@outlook.com>
2023-12-25 23:33:14 +0300

    Bottom configuration created

---
+++ HouseForSale_Api/Repositories/BottomGridRepositories/IBottomGridRepository.cs
@@ -0,0 +1,13 @@
+﻿using HouseForSale_Api.DTOs.BottomGridDtos;
+
+namespace HouseForSale_Api.Repositories.BottomGridRepositories
+{
+    public interface IBottomGridRepository
+    {
+        Task<List<ResultBottomGridDto>> GetAllBottomGridAsync();
+        void CreateBottomGrid(CreateBottomGridDto createBottomGridDto);
+        void DeleteBottomGrid(int id);
+        void UpdateBottomGrid(UpdateBottomGridDto updateBottomGridDto);
+        Task<GetBottomGridDto> GetBottomGrid(int id);
+    }
+}
