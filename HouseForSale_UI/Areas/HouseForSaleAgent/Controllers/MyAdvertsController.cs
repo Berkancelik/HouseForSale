5c766a1447d28f9baadd3f27f5a0a4b259cbf234
berkancelik <celikberkan1@outlook.com>
2025-01-13 00:32:39 +0300

    House For Sale Agent Layout Created

---
+++ HouseForSale_UI/Areas/HouseForSaleAgent/Controllers/MyAdvertsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HouseForSale_UI.Areas.HouseForSaleAgent.Controllers
+{
+    [Area("HouseForSaleAgent")]
+    public class MyAdvertsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HouseForSale_UI/DTOs/HouseForSaleAgentDtos/ResultHouseForSaleAgentDashboardChartDto.cs
@@ -0,0 +1,8 @@
+﻿namespace HouseForSale_UI.DTOs.HouseForSaleAgentDtos
+{
+    public class ResultHouseForSaleAgentDashboardChartDto
+    {
+        public int CityCount { get; set; }
+        public string City { get; set; }
+    }
+}
