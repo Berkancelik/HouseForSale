e1d0d1b8100fc77b1a68d93c7a4e560ef70e0916
berkancelik <celikberkan1@outlook.com>
2025-01-13 00:01:18 +0300

    RolesType Enum created

---
+++ HouseForSale_Api/Enums/RolesType.cs
@@ -0,0 +1,13 @@
+﻿namespace HouseForSale_Api.RolesTypes
+{
+    public enum RolesType
+    {
+        Member = 1,
+        Admin = 2,
+        Manager = 3,
+        Visitor = 4,
+        Employee = 5
+
+
+    }
+}
