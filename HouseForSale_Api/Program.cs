1cb1b8dc9f43562fb1ab63f1e55ee33e4cd6ccca
berkancelik <celikberkan1@outlook.com>
2025-01-14 22:36:16 +0300

    Extensions completed

--- HouseForSale_Api/Program.cs
+++ HouseForSale_Api/Program.cs
@@ -1,60 +1,10 @@
+using HouseForSale_Api.Containers;
 using HouseForSale_Api.Hubs;
 using HouseForSale_Api.Models.DapperContext;
-using HouseForSale_Api.Repositories.AppUserRepositories.Abstract;
-using HouseForSale_Api.Repositories.AppUserRepositories.Concrete;
-using HouseForSale_Api.Repositories.BottomGridRepository.Abstract;
-using HouseForSale_Api.Repositories.BottomGridRepository.Concrete;
-using HouseForSale_Api.Repositories.CategoryRepository.Abstract;
-using HouseForSale_Api.Repositories.CategoryRepository.Concrete;
-using HouseForSale_Api.Repositories.ContactRepository.Abstract;
-using HouseForSale_Api.Repositories.ContactRepository.Concrete;
-using HouseForSale_Api.Repositories.EmployeeRepositories.Abstract;
-using HouseForSale_Api.Repositories.EmployeeRepositories.Concrete;
-using HouseForSale_Api.Repositories.HouseForSaleRepositories.DashborardRepositories.ChartRepositories.Abstract;
-using HouseForSale_Api.Repositories.HouseForSaleRepositories.DashborardRepositories.ChartRepositories.Concrete;
-using HouseForSale_Api.Repositories.HouseForSaleRepositories.DashborardRepositories.LastProductsRepositories.Abstract;
-using HouseForSale_Api.Repositories.HouseForSaleRepositories.DashborardRepositories.LastProductsRepositories.Concrete;
-using HouseForSale_Api.Repositories.HouseForSaleRepositories.DashborardRepositories.StatisticRepositories.Abstract;
-using HouseForSale_Api.Repositories.HouseForSaleRepositories.DashborardRepositories.StatisticRepositories.Concrete;
-using HouseForSale_Api.Repositories.MessageRepositories.Abstract;
-using HouseForSale_Api.Repositories.MessageRepositories.Concrete;
-using HouseForSale_Api.Repositories.MessageRepository.Abstract;
-using HouseForSale_Api.Repositories.MessageRepository.Concrete;
-using HouseForSale_Api.Repositories.ProductImageRepositories.Abstract;
-using HouseForSale_Api.Repositories.ProductImageRepositories.Concrete;
-using HouseForSale_Api.Repositories.PropertyAmenityRepository.Abstract;
-using HouseForSale_Api.Repositories.PropertyAmenityRepository.Concrete;
-using HouseForSale_Api.Repositories.ServiceRepository.Abstract;
-using HouseForSale_Api.Repositories.ServiceRepository.Concrete;
-using HouseForSale_Api.Repositories.StatisticsRepository.Abstract;
-using HouseForSale_Api.Repositories.StatisticsRepository.Concrete;
-using HouseForSale_Api.Repositories.SubFeatureRepository.Abstract;
-using HouseForSale_Api.Repositories.SubFeatureRepository.Concrete;
-using HouseForSale_Api.Repositories.TestimonialRepository.Abstract;
-using HouseForSale_Api.Repositories.TestimonialRepository.Concrete;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddTransient<Context>();
-builder.Services.AddTransient<ICategoryRepository,CategoryRepository>();
-builder.Services.AddTransient<IProductRepository,ProductRepository>();
- builder.Services.AddTransient<IServiceRepository,ServiceRepository>();
-builder.Services.AddTransient<IBottomGridRepository, BottomGridRepository>();
-builder.Services.AddTransient<IPopularLocationRepository, PopularLocationRepository>();
-builder.Services.AddTransient<ITestimonialRepository, TestimonialRepository>();
-builder.Services.AddTransient<IEmployeeRepository, EmployeeRepository>();
-builder.Services.AddTransient<IStatisticsRepository, StatisticsRepository>();
-builder.Services.AddTransient<IWhoWeAreDetailRepository, WhoWeAreDetailRepository>();
-builder.Services.AddTransient<IContactRepository, ContactRepository>();
-builder.Services.AddTransient<IChartRepository, ChartRepository>();
-builder.Services.AddTransient<ILast5ProductsRepository, Last5ProductsRepository>();
-builder.Services.AddTransient<IStatisticRepository, StatisticRepository>();
-builder.Services.AddTransient<IMessageRepository, MessageRepository>();
-builder.Services.AddTransient<IPropertyAmenityRepository, PropertyAmenityRepository>();
-builder.Services.AddTransient<IProductImageRepository, ProductImageRepository>();
-builder.Services.AddTransient<IAppUserRepository, AppUserRepository>();
-builder.Services.AddTransient<ISubFeatureRepository, SubFeatureRepository>();
+builder.Services.ContainerDependencies();
+ 
  
 
 builder.Services.AddCors(option =>
